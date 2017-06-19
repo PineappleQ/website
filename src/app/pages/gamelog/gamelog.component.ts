4ee75db36e24ee43dce335259614a374d1ceacc8
pineappleQ <763297515@qq.com>
2017-06-19 16:11:22 +0800

    重新打包

--- src/app/pages/gamelog/gamelog.component.ts
+++ src/app/pages/gamelog/gamelog.component.ts
@@ -1,4 +1,5 @@
-import { NavController } from 'ionic-angular';
+import { UserService } from './../../service/userService';
+import { NavController, AlertController } from 'ionic-angular';
 import { Component } from '@angular/core';
 @Component({
     selector: 'game-log',
@@ -7,9 +8,43 @@ import { Component } from '@angular/core';
 })
 export class GameLogComponent {
     constructor(
-        public nav: NavController
+        public nav: NavController,
+        private userSvr: UserService,
+        private alertCtrl: AlertController
     ) { }
-    
+
+    usergameLog = {};
+
+    ionViewWillEnter() {
+        this.getGameLog();
+    }
+
+    getGameLog() {
+        this.userSvr.getUserBets().subscribe(
+            data => {
+                let result = data.json();
+                if (!result.error) {
+                    this.usergameLog = result.data;
+                } else {
+                    let msg = result.message ? result.message : "获取游戏记录失败";
+                    let alert = this.alertCtrl.create({
+                        message: msg
+                    });
+                    alert.present();
+                }
+            },
+            error => {
+                this.userSvr.errorHandler(error, (msg) => {
+                    let alert = this.alertCtrl.create({
+                        message: msg
+                    });
+                    alert.present();
+                    this.goBack();
+                }, "获取游戏记录失败");
+            }
+        )
+    }
+
     goBack() {
         this.nav.pop();
     }
